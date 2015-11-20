4f00968fa2a4b25e172e54c134e3fa9527515fe1
Yoshihiro Seki <nikochan2k@gmail.com>
2015-11-21 00:45:05 +0900

    Change Bits class for detecting big endian or little endian

--- src/Bits.ts
+++ src/Bits.ts
@@ -1,11 +1,29 @@
 import BitsType from "./BitsType";
 
 abstract class Bits {
+    protected static isBigEndian: boolean;
+
+    static initialize(): void {
+        if (Bits.isBigEndian == null) {
+            const buf = new ArrayBuffer(2);
+            const uint16 = new Uint16Array(buf);
+            uint16[0] = 1;
+            const uint8 = new Uint8Array(buf);
+            Bits.isBigEndian = (uint8[0] === 0);
+        }
+    }
+
     protected bitsType: BitsType;
+    protected buffer: ArrayBuffer;
 
     public get BitsType(): BitsType {
         return this.bitsType;
     }
+
+    public get Buffer() {
+        return this.buffer;
+    }
 }
+Bits.initialize();
 
 export default Bits;
