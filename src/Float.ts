805fa88fcc937ba8c9c22d897c22d7e4259f28de
Yoshihiro Seki <nikochan2k@gmail.com>
2015-11-29 14:56:00 +0900

    Modify Float class

--- src/Float.ts
+++ src/Float.ts
@@ -12,18 +12,18 @@ class Float extends Number {
             ? new Float64Array(platform) : new Float32Array(platform);
         f[0] = value;
         const u8platform = new Uint8Array(platform);
-        this._u8 = this.swap(u8platform);
+        this._u8 = this._toNetworkByteOrder(u8platform);
     }
 
     setBuffer(buffer: ArrayBuffer) {
         this._u8 = new Uint8Array(buffer);
-        const u8platform = this.swap(this._u8);
+        const u8platform = this._toNetworkByteOrder(this._u8);
         const f = this._byteLength === 8
             ? new Float64Array(u8platform.buffer) : new Float32Array(u8platform.buffer);
         this._value = f[0];
     }
 
-    protected swap(u8platform: Uint8Array): Uint8Array {
+    protected _toNetworkByteOrder(u8platform: Uint8Array): Uint8Array {
         if (Number._isBigEndian) {
             return u8platform;
         }
@@ -31,12 +31,12 @@ class Float extends Number {
         const end = this._byteLength / 2;
         for (let lhs = 0; lhs < end; lhs++) {
             const rhs = this._byteLength - lhs - 1;
-            this.swapByte(u8platform, lhs, rhs);
+            this._swap(u8platform, lhs, rhs);
         }
         return u8platform;
     }
 
-    protected swapByte(u8platform: Uint8Array, lhs: number, rhs: number): void {
+    protected _swap(u8platform: Uint8Array, lhs: number, rhs: number): void {
         const temp = u8platform[lhs];
         u8platform[lhs] = u8platform[rhs];
         u8platform[rhs] = temp;
